4d0e8ce789695f240d62c5227b0de7c7eff64503
Isaac Mthimunye <imthimun@c4r6s5.wethinkcode.co.za>
2020-07-16 09:03:53 +0200

    had to fix the end of dest

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: imthimun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/15 17:23:48 by imthimun          #+#    #+#             */
-/*   Updated: 2020/07/16 08:40:05 by imthimun         ###   ########.fr       */
+/*   Updated: 2020/07/16 09:02:44 by imthimun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char *ft_strcpy(char *dest, char *src)
 		dest[curr_char] = src[curr_char];
 		curr_char++;
 	}
-	dest[curr_char] = '\0'
+	dest[curr_char] = '\0';
 	return dest;
 }
 
